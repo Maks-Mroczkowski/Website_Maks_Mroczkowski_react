bb7f4a24806a4ec173006596f37ba8dba298e01f
Maks-Mroczkowski <161585131+Maks-Mroczkowski@users.noreply.github.com>
2025-05-09 21:18:22 +0100

    Animation for loading screen
    implementation of loading screen animation

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,10 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { LoadingScreen } from "./components/LoadingScreen.1";
+import "./index.css"
 
 function App() {
-  
+  const [isloaded, setIsLoaded] = useState(false)
+  return !isloaded && <><LoadingScreen onComplete={() => setIsLoaded(true)}/>
+  </>
 }
 
 export default App
